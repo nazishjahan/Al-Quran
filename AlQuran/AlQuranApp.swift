020a667bcbc7e76a4dfc95826994c4a91965e891
nazish jahan <nazishjahan04@yahoo.com>
2021-06-08 16:27:48 +0500

    Initial Commit

---
+++ AlQuran/AlQuranApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlQuranApp.swift
+//  AlQuran
+//
+//  Created by NAZISH ZOHAIB on 06/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct AlQuranApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
